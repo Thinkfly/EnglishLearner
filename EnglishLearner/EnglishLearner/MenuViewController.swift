7d00fc1caff3d24e8a727eeb828bcf84621fbf5e
weirunsu <weirunsu@fitstime.com>
2015-06-15 17:33:53 +0800

    Update

--- EnglishLearner/EnglishLearner/MenuViewController.swift
+++ EnglishLearner/EnglishLearner/MenuViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 import SnapKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDataSource {
     
-    var bookTableView : UITableView = UITableView()
-    var mediaTableView : UITableView = UITableView()
-    var getDataButton : UIButton = UIButton()
+    let IDENTIFIER_TABLEVIEW_BOOK = "IDENTIFIER_TABLEVIEW_BOOK"
+    let IDENTIFIER_TABLEVIEW_MEDIA = "IDENTIFIER_TABLEVIEW_MEDIA"
+    
+    let tableView : UITableView = UITableView()
+//    var mediaTableView : UITableView = UITableView()
+    let getDataButton : UIButton = UIButton()
+    
+    var bookArray : NSMutableArray?
+    var mediaArray : NSMutableArray?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,24 +35,6 @@ class MenuViewController: UIViewController {
         
         let tableHeight = (Constant.screenHeight - spHeight * 3 - buttonHeight) / 2
         
-        self.view.addSubview(bookTableView)
-        bookTableView.backgroundColor = UIColor.brownColor()
-        bookTableView.snp_makeConstraints { (make) -> Void in
-            make.top.equalTo(self.view).offset(20)
-            make.left.equalTo(self.view).offset(0)
-            make.right.equalTo(self.view).offset(0)
-            make.height.equalTo(tableHeight)
-        }
-        
-        self.view.addSubview(mediaTableView)
-        mediaTableView.backgroundColor = UIColor.orangeColor()
-        mediaTableView.snp_makeConstraints { (make) -> Void in
-            make.top.equalTo(bookTableView.snp_bottom).offset(20)
-            make.left.equalTo(self.view).offset(0)
-            make.right.equalTo(self.view).offset(0)
-            make.height.equalTo(tableHeight)
-        }
-
         self.view.addSubview(getDataButton)
         getDataButton.backgroundColor = UIColor(red: CGFloat(3.0 / 255.0), green: CGFloat(192.0 / 255.0), blue: CGFloat(198.0 / 255.0), alpha: CGFloat(1.0))
         getDataButton.titleLabel?.textColor = UIColor.whiteColor()
@@ -58,6 +46,29 @@ class MenuViewController: UIViewController {
             make.centerX.equalTo(self.view)
             make.height.equalTo(50)
         }
+        getDataButton.addTarget(self, action: Selector("getData:"), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(tableView)
+        tableView.backgroundColor = UIColor.brownColor()
+        tableView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view).offset(20)
+            make.left.equalTo(self.view).offset(0)
+            make.right.equalTo(self.view).offset(0)
+            make.bottom.equalTo(self.getDataButton.snp_top).offset(-20)
+        }
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        
+//        self.view.addSubview(mediaTableView)
+//        mediaTableView.backgroundColor = UIColor.orangeColor()
+//        mediaTableView.snp_makeConstraints { (make) -> Void in
+//            make.top.equalTo(bookTableView.snp_bottom).offset(20)
+//            make.left.equalTo(self.view).offset(0)
+//            make.right.equalTo(self.view).offset(0)
+//            make.height.equalTo(tableHeight)
+//        }
+
+        
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -80,8 +91,135 @@ class MenuViewController: UIViewController {
     }
     */
     
+    //获取本地数据按钮
     @IBAction func getData(sender : UIButton){
+        //self.getData()
+        self.getDeviceResourceData("")
+    }
+    
+    //获取本地数据
+    func getData() {
         
+        
+        
+        
+    }
+    
+    func getDeviceResourceData(fileName : String) {
+        let pathArray : NSArray = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        ///Users/app/Library/Developer/CoreSimulator/Devices/9AE9A419-D4B2-4BD2-96A2-6C432D2BDFB7/data/Containers/Data/Application/58585DCF-B506-4EAC-976C-91675CE0245E/Documents
+        if (0 < pathArray.count) {
+            var pathStr : String = pathArray.objectAtIndex(0) as! String
+            
+            if (!fileName.isEmpty) {
+                pathStr = "\(pathStr)/\(fileName)"
+            }
+            
+            let fileManager : NSFileManager = NSFileManager.defaultManager()
+            
+            let tempFileList : NSArray = NSArray(array: fileManager.contentsOfDirectoryAtPath(pathStr, error: nil)!)
+            
+            for name in tempFileList {
+                let filePath = "\(pathStr)/\(name)"
+                
+                var isDir = ObjCBool(false)
+                
+                var isExist : Bool = fileManager.fileExistsAtPath(filePath, isDirectory: &isDir)
+                
+                if (isExist && isDir) {
+                    self.getDeviceResourceData(name as! String)
+                } else {
+                    switch (filePath.pathExtension) {
+                    case "pdf" :
+                        self.bookArray?.addObject(filePath)
+                        break
+                        
+                    case "mp3" :
+                        self.mediaArray?.addObject(filePath)
+                        break
+                        
+                    default:
+                        break
+                    }
+                    
+                    
+                }
+                
+            }
+            
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (0 == section) {
+            return "图书列表"
+        } else {
+            return "音频列表"
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (0 == section) {
+            return 1
+        } else {
+            return 1
+        }
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell : AnyObject? = nil
+        
+        if (0 == indexPath.section) {
+            
+            cell = tableView.dequeueReusableCellWithIdentifier(IDENTIFIER_TABLEVIEW_BOOK)
+            
+            var bookTableViewCell : BookTableViewCell!
+            
+            if let tableViewCell : AnyObject = cell {
+                
+                bookTableViewCell = tableViewCell as! BookTableViewCell
+                
+            } else {
+                
+                bookTableViewCell = BookTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: IDENTIFIER_TABLEVIEW_BOOK)
+                
+                cell = bookTableViewCell
+                
+            }
+            
+            bookTableViewCell.label.text = "你好"
+            
+        } else {
+            
+            cell = tableView.dequeueReusableCellWithIdentifier(IDENTIFIER_TABLEVIEW_BOOK)
+            
+            var bookTableViewCell : BookTableViewCell!
+            
+            if let tableViewCell : AnyObject = cell {
+                
+                bookTableViewCell = tableViewCell as! BookTableViewCell
+                
+            } else {
+                
+                bookTableViewCell = BookTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: IDENTIFIER_TABLEVIEW_BOOK)
+                
+                cell = bookTableViewCell
+                
+            }
+            
+            bookTableViewCell.label.text = "我好"
+            
+        }
+        
+        return cell as! UITableViewCell
     }
 
 }
---
+++ EnglishLearner/EnglishLearner/TableViewCell/BookTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  BookTableViewCell.swift
+//  EnglishLearner
+//
+//  Created by app on 15/6/15.
+//  Copyright (c) 2015年 thinkfly. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class BookTableViewCell: UITableViewCell {
+    
+    var label : UILabel!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.initSubViews()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    //初始化子视图
+    func initSubViews() {
+        label = UILabel()
+        label.backgroundColor = UIColor.blueColor()
+        self.contentView .addSubview(label)
+        
+        label.snp_makeConstraints { (make) -> Void in
+            make.centerY.equalTo(self.contentView.snp_centerY)
+            make.leading.equalTo(self.contentView).offset(0)
+            make.trailing.equalTo(self.contentView).offset(0)
+        }
+    }
+
+}
